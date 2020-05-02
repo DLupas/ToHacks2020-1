936e96d85a07c52c19e2697921477a8d287b21f1
Rishbob <43894878+Rishbob@users.noreply.github.com>
2020-05-02 15:35:13 -0400

    Add files via upload
    First iteration of tensorflow cases (ONLY FOR TESTING PURPOSES)

---
+++ Tensorflow Cases.py
@@ -0,0 +1,4 @@
+{"Responses": [
+    {"tag": "check", "patterns": ["Do you work?", "Are you functional?", "Are you brain dead?"],
+     "responses": ["Of course not boss!", "I have brain cancer, of course I'm not!", "The real question is: are you?"]}
+]}
